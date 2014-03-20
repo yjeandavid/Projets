cc9c0310cb56a7f73452ca45d3cf499c9fcf3faf
Michael <Michael@172.29.116.68>
2014-03-20 12:54:46 -0400

    suppression de codes non utilisés

--- Documents/NetBeansProjects/TP1/src/ca/uqam/inf2015/tp1/employe/Employe.java
+++ Documents/NetBeansProjects/TP1/src/ca/uqam/inf2015/tp1/employe/Employe.java
@@ -2,7 +2,6 @@ package ca.uqam.inf2015.tp1.employe;
 
 import ca.uqam.inf2015.tp1.application.AppConfig;
 import ca.uqam.inf2015.tp1.feuilleDeTemps.FeuilleDeTemps;
-import ca.uqam.inf2015.tp1.feuilleDeTemps.FeuilleDeTempsDDC2;
 import java.io.IOException;
 
 public class Employe {
@@ -34,140 +33,7 @@ public class Employe {
         }
 
         return messageValidation;
-    }
-
-    private String validerFeuilleDeTempsSelonType() throws IOException {
-        String messageValidation = "";
-
-        if (typeEmploye
-                == AppConfig.getParametreRetournerUnInt(
-                    "EMPLOYE_ADMINISTRATION")) {
-            messageValidation = validerSemaineTypeAdmin();
-        } else if (typeEmploye
-                   == AppConfig.getParametreRetournerUnInt(
-                       "EMPLOYE_EXPLOITATION")) {
-            messageValidation = validerSemaineTypeExploitation();
-        } else if (typeEmploye
-                   == AppConfig.getParametreRetournerUnInt(
-                       "EMPLOYE_PRODUCTION")) {
-            messageValidation = validerSemaineTypeProduction();
-        }
-
-        return messageValidation;
-    }
-
-    public String validerSemaineTypeAdmin() throws IOException {
-        String messageValidation = "";
-
-        messageValidation += validerJoursOuvrablesAdmin() + ',';
-        messageValidation += validerHeuresTravailBureauParSemaine(
-            AppConfig
-                .getParametreRetournerUnDouble(
-                    "MINIMUM_MINUTES_BUREAU_ADMIN_SEMAINE"), AppConfig
-                        .getParametreRetournerUnDouble(
-                            "MAXIMUM_MINUTES_BUREAU_ADMIN_SEMAINE")) + ',';
-        messageValidation += validerHeuresTeleTravailParSemaine(
-            AppConfig.getParametreRetournerUnDouble(
-                "MAXIMUM_MINUTES_TELE_TRAVAIL_ADMIN_SEMAINE")) + ',';
-        messageValidation += validerJoursCongesMaladies();
-        messageValidation += validerCongesFeries();
-        messageValidation += new FeuilleDeTempsDDC2(feuileDeTemps,
-                feuileDeTemps.getJours()).resultatFinalAdmin();
-
-        return messageValidation;
-    }
-
-    public String validerSemaineTypeExploitation() throws IOException {
-        String messageValidation = "";
-
-        messageValidation += validerJoursOuvrablesNormal() + ',';
-        messageValidation += validerHeuresTravailBureauParSemaine(
-            AppConfig
-                .getParametreRetournerUnDouble(
-                    "MINIMUM_MINUTES_BUREAU_EXPLOITATION_SEMAINE"), AppConfig
-                        .getParametreRetournerUnDouble(
-                            "MAXIMUM_MINUTES_BUREAU_EMPLOYE_SEMAINE")) + ',';
-        messageValidation += validerJoursCongesMaladies();
-        messageValidation += validerCongesFeries();
-        messageValidation += new FeuilleDeTempsDDC2(feuileDeTemps,
-                feuileDeTemps.getJours()).resultatFinalExploitation();
-
-        return messageValidation;
-    }
-
-    public String validerSemaineTypeProduction() throws IOException {
-        String messageValidation = "";
-
-        messageValidation += validerJoursOuvrablesNormal() + ',';
-        messageValidation += validerHeuresTravailBureauParSemaine(
-            AppConfig
-                .getParametreRetournerUnDouble(
-                    "MINIMUM_MINUTES_BUREAU_PRODUCTION_SEMAINE"), AppConfig
-                        .getParametreRetournerUnDouble(
-                            "MAXIMUM_MINUTES_BUREAU_EMPLOYE_SEMAINE")) + ',';
-        messageValidation += validerJoursCongesMaladies();
-        messageValidation += validerCongesFeries();
-        messageValidation += new FeuilleDeTempsDDC2(feuileDeTemps,
-                feuileDeTemps.getJours()).resultatFinalProduction();
-
-        return messageValidation;
     }*/
-
-    public String validerJoursOuvrablesAdmin() throws IOException {
-        return feuileDeTemps
-            .validerJoursOuvrables(
-                AppConfig
-                    .getParametreRetournerUnDouble(
-                        "MINIMUM_MINUTES_BUREAU_ADMIN_PAR_JOUR"), AppConfig
-                            .getParametreRetournerUnDouble(
-                                "MAXIMUM_MINUTES_OFFICE_WORK_BY_DAY"));
-    }
-
-    public String validerJoursOuvrablesNormal() throws IOException {
-        return feuileDeTemps.validerJoursOuvrables(
-                AppConfig.getParametreRetournerUnDouble("MINIMUM_MINUTES_BUREAU_PRODUCTION_PAR_JOUR"), 
-                AppConfig.getParametreRetournerUnDouble("MAXIMUM_MINUTES_OFFICE_WORK_BY_DAY"));
-    }
-
-    /*public String validerHeuresTravailBureauParSemaine(
-            double minimumHeuresDeBureauParSemaine,
-            double maximumHeuresDeBureauParSemaine)
-            throws IOException {
-        String messageValidation = "";
-
-        if (heuresDeBureauParSemaine < minimumHeuresDeBureauParSemaine) {
-            messageValidation += AppConfig.getParametreRetournerUnString(
-                "MSG_HEURES_MINIMUM_SEMAINE_BUREAU") + ',';
-        } else if (heuresDeBureauParSemaine > maximumHeuresDeBureauParSemaine) {
-            messageValidation += AppConfig.getParametreRetournerUnString(
-                "MSG_HEURES_MAXIMAL_BUREAU_SEMAINE") + ',';
-        }
-
-        return messageValidation;
-    }
-
-    public String validerHeuresTeleTravailParSemaine(
-            double maximumHeuresTeleTravailParSemaine)
-            throws IOException {
-        String messageValidation = "";
-
-        if (heuresTeleTravailParSemaine > maximumHeuresTeleTravailParSemaine) {
-            messageValidation += AppConfig.getParametreRetournerUnString(
-                "MSG_HEURES_MAXIMAL_TELE_TRAVAIL") + ',';
-        }
-
-        return messageValidation;
-    }*/
-
-    private String validerJoursCongesMaladies() throws IOException {
-        return feuileDeTemps.validerCongesMaladie(
-            AppConfig.getParametreRetournerUnDouble("MINUTES_CONGES_MALADIE"));
-    }
-
-    private String validerCongesFeries() throws IOException {
-        return feuileDeTemps.validerCongesFeries(
-            AppConfig.getParametreRetournerUnDouble("MINUTES_CONGES_FERIES"));
-    }
 
     public int getNoEmplye() {
         return noEmploye;
