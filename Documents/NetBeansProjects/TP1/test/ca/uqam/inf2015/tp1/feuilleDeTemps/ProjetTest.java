c2bfda4031cd3cd4ca5ef4a76c7f537af275100d
Michael <Michael@172.29.116.68>
2014-03-21 15:59:36 -0400

    Ajout des tests unitaires pour la classe Projet

--- Documents/NetBeansProjects/TP1/test/ca/uqam/inf2015/tp1/feuilleDeTemps/ProjetTest.java
+++ Documents/NetBeansProjects/TP1/test/ca/uqam/inf2015/tp1/feuilleDeTemps/ProjetTest.java
@@ -1,5 +1,7 @@
 package ca.uqam.inf2015.tp1.feuilleDeTemps;
 
+import ca.uqam.inf2015.tp1.application.AppConfig;
+import java.io.IOException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -13,7 +15,8 @@ public class ProjetTest {
     }
     
     @BeforeClass
-    public static void setUpClass() {
+    public static void setUpClass() throws IOException {
+        AppConfig.chargerParametres();
     }
     
     @AfterClass
@@ -33,98 +36,260 @@ public class ProjetTest {
         System.out.println("construireProjet");
         int noProjet = 0;
         double minutes = 0.0;
-        Projet expResult = null;
-        Projet result = Projet.construireProjet(noProjet, minutes);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Projet result = null;
+        result = Projet.construireProjet(noProjet, minutes);
+        assertNotNull(result);
     }
 
     @Test
     public void testGetNoProjet() {
         System.out.println("getNoProjet");
-        Projet instance = null;
-        int expResult = 0;
+        Projet instance = Projet.construireProjet(110, 300.0);
+        int expResult = 110;
         int result = instance.getNoProjet();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     @Test
     public void testGetMinutes() {
         System.out.println("getMinutes");
-        Projet instance = null;
-        double expResult = 0.0;
+        Projet instance = Projet.construireProjet(110, 300.0);
+        double expResult = 300.0;
         double result = instance.getMinutes();
         assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     @Test
     public void testEstTravailBureau() throws Exception {
         System.out.println("estTravailBureau");
-        Projet instance = null;
+        Projet instance = Projet.construireProjet(902, 500.0);
         boolean expResult = false;
         boolean result = instance.estTravailBureau();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        instance = Projet.construireProjet(125, 420.0);
+        expResult = true;
+        result = instance.estTravailBureau();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(999, 420.0);
+        expResult = false;
+        result = instance.estTravailBureau();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(900, 420.0);
+        expResult = true;
+        result = instance.estTravailBureau();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(998, 420.0);
+        expResult = false;
+        result = instance.estTravailBureau();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(997, 420.0);
+        expResult = false;
+        result = instance.estTravailBureau();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(996, 420.0);
+        expResult = false;
+        result = instance.estTravailBureau();
+        assertEquals(expResult, result);
     }
 
     @Test
     public void testEstTeleTravail() throws Exception {
         System.out.println("estTeleTravail");
-        Projet instance = null;
-        boolean expResult = false;
+        Projet instance = Projet.construireProjet(902, 500.0);
+        boolean expResult = true;
         boolean result = instance.estTeleTravail();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        instance = Projet.construireProjet(125, 420.0);
+        expResult = false;
+        result = instance.estTeleTravail();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(999, 420.0);
+        expResult = false;
+        result = instance.estTeleTravail();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(990, 420.0);
+        expResult = true;
+        result = instance.estTeleTravail();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(998, 420.0);
+        expResult = false;
+        result = instance.estTeleTravail();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(997, 420.0);
+        expResult = false;
+        result = instance.estTeleTravail();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(996, 420.0);
+        expResult = false;
+        result = instance.estTeleTravail();
+        assertEquals(expResult, result);
     }
 
     @Test
     public void testEstUnCongeMaladie() throws Exception {
         System.out.println("estUnCongeMaladie");
-        Projet instance = null;
+        Projet instance = Projet.construireProjet(902, 500.0);
         boolean expResult = false;
         boolean result = instance.estUnCongeMaladie();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        instance = Projet.construireProjet(125, 420.0);
+        expResult = false;
+        result = instance.estUnCongeMaladie();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(999, 420.0);
+        expResult = true;
+        result = instance.estUnCongeMaladie();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(990, 420.0);
+        expResult = false;
+        result = instance.estUnCongeMaladie();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(998, 420.0);
+        expResult = false;
+        result = instance.estUnCongeMaladie();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(997, 420.0);
+        expResult = false;
+        result = instance.estUnCongeMaladie();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(996, 420.0);
+        expResult = false;
+        result = instance.estUnCongeMaladie();
+        assertEquals(expResult, result);
     }
 
     @Test
     public void testEstUnCongeFerie() throws Exception {
         System.out.println("estUnCongeFerie");
-        Projet instance = null;
+        Projet instance = Projet.construireProjet(902, 500.0);
         boolean expResult = false;
         boolean result = instance.estUnCongeFerie();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        instance = Projet.construireProjet(125, 420.0);
+        expResult = false;
+        result = instance.estUnCongeFerie();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(999, 420.0);
+        expResult = false;
+        result = instance.estUnCongeFerie();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(990, 420.0);
+        expResult = false;
+        result = instance.estUnCongeFerie();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(998, 420.0);
+        expResult = true;
+        result = instance.estUnCongeFerie();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(997, 420.0);
+        expResult = false;
+        result = instance.estUnCongeFerie();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(996, 420.0);
+        expResult = false;
+        result = instance.estUnCongeFerie();
+        assertEquals(expResult, result);
     }
 
     @Test
     public void testEstJourneeVacance() throws Exception {
         System.out.println("estJourneeVacance");
-        Projet instance = null;
+        Projet instance = Projet.construireProjet(902, 500.0);
         boolean expResult = false;
         boolean result = instance.estJourneeVacance();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        instance = Projet.construireProjet(125, 420.0);
+        expResult = false;
+        result = instance.estJourneeVacance();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(999, 420.0);
+        expResult = false;
+        result = instance.estJourneeVacance();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(990, 420.0);
+        expResult = false;
+        result = instance.estJourneeVacance();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(998, 420.0);
+        expResult = false;
+        result = instance.estJourneeVacance();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(997, 420.0);
+        expResult = true;
+        result = instance.estJourneeVacance();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(996, 420.0);
+        expResult = false;
+        result = instance.estJourneeVacance();
+        assertEquals(expResult, result);
     }
 
     @Test
     public void testEstUnCongeParental() throws Exception {
         System.out.println("estUnCongeParental");
-        Projet instance = null;
+        Projet instance = Projet.construireProjet(902, 500.0);
         boolean expResult = false;
         boolean result = instance.estUnCongeParental();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        instance = Projet.construireProjet(125, 420.0);
+        expResult = false;
+        result = instance.estUnCongeParental();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(999, 420.0);
+        expResult = false;
+        result = instance.estUnCongeParental();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(990, 420.0);
+        expResult = false;
+        result = instance.estUnCongeParental();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(998, 420.0);
+        expResult = false;
+        result = instance.estUnCongeParental();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(997, 420.0);
+        expResult = false;
+        result = instance.estUnCongeParental();
+        assertEquals(expResult, result);
+        
+        instance = Projet.construireProjet(996, 420.0);
+        expResult = true;
+        result = instance.estUnCongeParental();
+        assertEquals(expResult, result);
     }
 }
